e12ffe04620bd4ae3e95a0c157802e068654c84b
y-du <42994541+y-du@users.noreply.github.com>
2025-07-02 08:05:20 +0200

    move

---
+++ cert-manager/pkg/models/cert/cert.go
@@ -0,0 +1,27 @@
+package cert
+
+import "time"
+
+type Info struct {
+	Version            int               `json:"version"`
+	SerialNumber       string            `json:"serial_number"`
+	NotBefore          time.Time         `json:"not_before"`
+	NotAfter           time.Time         `json:"not_after"`
+	SignatureAlgorithm string            `json:"signature_algorithm"`
+	PublicKeyAlgorithm string            `json:"public_key_algorithm"`
+	Issuer             DistinguishedName `json:"issuer"`
+	Subject            DistinguishedName `json:"subject"`
+	SubjectAltNames    []string          `json:"subject_alt_names"`
+}
+
+type DistinguishedName struct {
+	Country            []string `json:"country"`
+	Organization       []string `json:"organization"`
+	OrganizationalUnit []string `json:"organizational_unit"`
+	Locality           []string `json:"locality"`
+	Province           []string `json:"province"`
+	StreetAddress      []string `json:"street_address"`
+	PostalCode         []string `json:"postal_code"`
+	SerialNumber       string   `json:"serial_number"`
+	CommonName         string   `json:"common_name"`
+}
