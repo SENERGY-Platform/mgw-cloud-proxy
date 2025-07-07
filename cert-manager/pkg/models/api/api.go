352238aa354cd647279c722f071e52020b4bfce0
y-du <42994541+y-du@users.noreply.github.com>
2025-07-07 14:44:06 +0200

    add request models

--- cert-manager/pkg/models/api/api.go
+++ cert-manager/pkg/models/api/api.go
@@ -1,7 +1,27 @@
 package api
 
+import (
+	models_cert "github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/cert"
+)
+
 const (
 	HeaderRequestID = "X-Request-ID"
 	HeaderApiVer    = "X-Api-Version"
 	HeaderSrvName   = "X-Service-Name"
 )
+
+type NewNetworkRequest struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type NewCertRequest struct {
+	models_cert.DistinguishedName
+	ValidityPeriod string `json:"validity_period"`
+	PrivateKey     string `json:"private_key"`
+}
+
+type RenewCertRequest struct {
+	models_cert.DistinguishedName
+	ValidityPeriod string `json:"validity_period"`
+}
