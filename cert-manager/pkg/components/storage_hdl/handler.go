40a379acac84736da1190bb2cbe9024282d51b6a
y-du <42994541+y-du@users.noreply.github.com>
2025-07-07 10:22:04 +0200

    add storage handler

---
+++ cert-manager/pkg/components/storage_hdl/handler.go
@@ -0,0 +1,124 @@
+package storage_hdl
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/components/file_util"
+	models_error "github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/error"
+	models_storage "github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/storage"
+	"os"
+	"path"
+	"sync"
+)
+
+const (
+	certDataFile = "cert_data.json"
+	netDataFile  = "net_data.json"
+)
+
+type Handler struct {
+	workDirPath string
+	mu          sync.RWMutex
+}
+
+func New(workDirPath string) *Handler {
+	return &Handler{workDirPath: workDirPath}
+}
+
+func (h *Handler) Init() error {
+	return os.MkdirAll(h.workDirPath, 0666)
+}
+
+func (h *Handler) ReadCertificate(_ context.Context) (models_storage.CertData, error) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	var data models_storage.CertData
+	if err := read(path.Join(h.workDirPath, certDataFile), &data); err != nil {
+		if os.IsNotExist(err) {
+			return models_storage.CertData{}, models_error.NewNotFoundError(errors.New("no certificate data"))
+		}
+		return models_storage.CertData{}, models_error.NewInternalError(err)
+	}
+	return data, nil
+}
+
+func (h *Handler) ReadNetwork(_ context.Context) (models_storage.NetworkData, error) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	var data models_storage.NetworkData
+	if err := read(path.Join(h.workDirPath, netDataFile), &data); err != nil {
+		if os.IsNotExist(err) {
+			return models_storage.NetworkData{}, models_error.NewNotFoundError(errors.New("no network data"))
+		}
+		return models_storage.NetworkData{}, models_error.NewInternalError(err)
+	}
+	return data, nil
+}
+
+func (h *Handler) WriteCertificate(_ context.Context, data models_storage.CertData) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if err := write(path.Join(h.workDirPath, certDataFile), data); err != nil {
+		return models_error.NewInternalError(err)
+	}
+	return nil
+}
+
+func (h *Handler) WriteNetwork(_ context.Context, data models_storage.NetworkData) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if err := write(path.Join(h.workDirPath, netDataFile), data); err != nil {
+		return models_error.NewInternalError(err)
+	}
+	return nil
+}
+
+func (h *Handler) RemoveCertificate(_ context.Context) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	return os.Remove(path.Join(h.workDirPath, certDataFile))
+}
+
+func (h *Handler) RemoveNetwork(_ context.Context) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	return os.Remove(path.Join(h.workDirPath, netDataFile))
+}
+
+func read(filePath string, data any) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if err = json.NewDecoder(file).Decode(data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func write(filePath string, data any) error {
+	fileBkPath, err := file_util.BackupFile(filePath, 0664)
+	if err != nil {
+		return err
+	}
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		file.Close()
+		if err != nil && fileBkPath != "" {
+			if e := file_util.Copy(fileBkPath, filePath, 0664); e != nil {
+				err = errors.Join(err, e)
+			}
+		}
+	}()
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	if err = encoder.Encode(data); err != nil {
+		return err
+	}
+	return nil
+}
