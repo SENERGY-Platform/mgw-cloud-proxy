8c5bdb2b067cbd988f78393fb234dece85a19a8d
y-du <42994541+y-du@users.noreply.github.com>
2025-07-08 10:45:26 +0200

    add import alias

--- cert-manager/pkg/models/service/service.go
+++ cert-manager/pkg/models/service/service.go
@@ -1,13 +1,13 @@
 package service
 
 import (
-	"github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/cert"
+	models_cert "github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/cert"
 	models_storage "github.com/SENERGY-Platform/mgw-cloud-proxy/pkg/models/storage"
 	"time"
 )
 
 type CertInfo struct {
-	cert.Info
+	models_cert.Info
 	models_storage.CertData
 	LastChecked time.Time `json:"last_checked"`
 }
