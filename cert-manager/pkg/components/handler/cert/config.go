aabb0d721ffefddf8aff3feb1b01ac4440e31759
y-du <42994541+y-du@users.noreply.github.com>
2025-07-14 16:03:00 +0200

    remove

--- cert-manager/pkg/components/handler/cert/config.go
+++ cert-manager/pkg/components/handler/cert/config.go
@@ -1,11 +1,8 @@
 package cert
 
-import "time"
-
 type Config struct {
-	WorkDirPath           string        `json:"work_dir_path" env_var:"CERT_HDL_WORKDIR"`
-	TargetDirPath         string        `json:"target_dir_path" env_var:"CERT_HDL_TARGET_DIR"`
-	DummyDirPath          string        `json:"dummy_dir_path" env_var:"CERT_HDL_DUMMY_DIR"`
-	PrivateKeyAlgorithm   string        `json:"private_key_algorithm" env_var:"PRIVATE_KEY_ALGO"`
-	DefaultValidityPeriod time.Duration `json:"default_validity_period" env_var:"CERT_VALIDITY_PERIOD"`
+	WorkDirPath         string `json:"work_dir_path" env_var:"CERT_HDL_WORKDIR"`
+	TargetDirPath       string `json:"target_dir_path" env_var:"CERT_HDL_TARGET_DIR"`
+	DummyDirPath        string `json:"dummy_dir_path" env_var:"CERT_HDL_DUMMY_DIR"`
+	PrivateKeyAlgorithm string `json:"private_key_algorithm" env_var:"PRIVATE_KEY_ALGO"`
 }
